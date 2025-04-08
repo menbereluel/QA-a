bb58bd39e6cb9eb2945b3d53939e3d662d9c8c01
Menbere Luel <menbereluelk@gmail.com>
2025-04-08 21:52:29 +0300

    created test

---
+++ fizzbuzz.test.js
@@ -0,0 +1,12 @@
+const fizzBuzz =require('./fizzbuzz')
+
+test("",()=>{
+    expect(fizzBuzz(60)).toBe('FizzBuzz');
+
+});
+test("",()=>{
+    expect(fizzBuzz(3)).toBe('Fizz');
+
+});
+test("",()=>{
+    expect(fizzBuzz(5)).toBe('Buzz');});
---
+++ numbers.test.js
@@ -0,0 +1,4 @@
+const isEven = require('./numbers')
+test("even",()=>{
+    expect(isEven(9)).toBe(false);
+});
